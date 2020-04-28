89fe8a69a169e5ef5552440697289f4901c82911
ignacio68 <ilopez-amor@ya.com>
2020-04-28 19:37:28 +0200

    Improve: add tests

---
+++ app/tests/example.ts
@@ -0,0 +1,11 @@
+// A sample QUnit test
+// const QUnit = require('qunit')
+import QUnit from '@types/qunit'
+// import { Assert } from '@types/qunit'
+
+QUnit.test("equal test", function (assert) {
+  assert.equal( 0, 0, "Zero, Zero; equal succeeds" );
+  assert.equal( "", 0, "Empty, Zero; equal succeeds" );
+  assert.equal( "", "", "Empty, Empty; equal succeeds" );
+  assert.equal( 0, false, "Zero, false; equal succeeds" );
+});
