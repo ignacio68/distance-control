b61c5d4065dd1fb781aeb73c7819ec34bd417a18
ignacio68 <ilopez-amor@ya.com>
2020-05-06 08:12:59 +0200

    export state

--- app/store/geolocation.ts
+++ app/store/geolocation.ts
@@ -1,7 +1,7 @@
 import Vue from 'Vue'
 import { UserLocation } from './types'
 
-const state = Vue.observable({
+export const state = Vue.observable({
     userLocation: {
         latitude: '',
         longitude: ''
