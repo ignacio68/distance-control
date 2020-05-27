604ccf54c6a42f7d623a7e717f589e08d61d2aac
Ignacio <ilopez-amor@ya.com>
2020-05-27 10:25:07 +0200

    Fix units bug

--- app/utils/circle.ts
+++ app/utils/circle.ts
@@ -2,7 +2,7 @@ import { PI, range, degreesToRadians as radians } from './core'
 
 import { Coordinates, Circle, GetCoordinates } from './types'
 
-const EARTH_RADIUS = 6378000 // meters
+const EARTH_RADIUS = 6378 // km
 
 const getCoordinates = (options: GetCoordinates) => {
   const { theta, radius, lat, lng } = options
@@ -10,6 +10,7 @@ const getCoordinates = (options: GetCoordinates) => {
   const dy = Math.sin(radians(theta)) * radius
   const dx = Math.cos(radians(theta)) * radius
   const newLatitude = lat + (dy / EARTH_RADIUS) * (180 / PI)
+  console.log(`getCoordinates().newLatitude: ${newLatitude}`)
   const newLongitude =
     lng + ((dx / EARTH_RADIUS) * (180 / PI)) / Math.cos(radians((lat * PI) / 180))
   const coordinates: Coordinates = { lat: newLatitude, lng: newLongitude }
@@ -21,6 +22,8 @@ const getCircleCoordinates = (options: Circle) => {
 
   const lat = center.lat
   const lng = center.lng
+  console.log(`getCircleCoordinates lat: ${lat}`)
+
   const thetaList = range(0, 360, 360 / numberOfPoints)
   const circleCoordinates: Coordinates[] = thetaList.map((theta) => {
     const circleCoordinates = getCoordinates({ theta, radius, lat, lng })
