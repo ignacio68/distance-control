0e197bc1d7b624c6dbba0bbdb1ee5f3cba51e5d3
Ignacio <ilopez-amor@ya.com>
2020-05-27 10:19:08 +0200

    Add security area state

--- app/store/userLocation.ts
+++ app/store/userLocation.ts
@@ -3,19 +3,42 @@ import { UserLocation } from './types'
 
 export const state = Vue.observable({
     userLocation: {
-        lat: '',
-        lng: ''
+        lat: 0,
+        lng: 0
     },
     userCurrentLocation: {
-        lat: '',
-        lng: ''
-    }
+        lat: 0,
+        lng: 0
+    },
+    isSecurityArea: false
 })
 
+export const getOriginLocation = () => { 
+    const originLocation = {
+        lat: String(state.userLocation.lat),
+        lng: String(state.userLocation.lng)
+    }
+    return originLocation
+}
 export const getUserLocation = () => state.userLocation
 export const getUserCurrentLocation = () => state.userCurrentLocation
+export const getIsSecurityArea = () => state.isSecurityArea
 
-export const setUserLocation = (coordinates: UserLocation) => state.userLocation = coordinates
+export const setUserLocation = (coordinates: UserLocation) => {
+    // Object.assign({state}, coordinates) // TODO: Change coordinates name to userLocation
+    state.userLocation.lat = coordinates.lat
+    state.userLocation.lng = coordinates.lng
+}
 export const setUserCurrentLocation = (coordinates: UserLocation) => state.userCurrentLocation = coordinates
+export const setIsSecurityArea = !state.isSecurityArea
 
-export default { state, getUserLocation, getUserCurrentLocation, setUserLocation, setUserCurrentLocation }
+export default {
+    state,
+    getOriginLocation,
+    getUserLocation,
+    getUserCurrentLocation,
+    getIsSecurityArea,
+    setUserLocation,
+    setUserCurrentLocation,
+    setIsSecurityArea
+}
