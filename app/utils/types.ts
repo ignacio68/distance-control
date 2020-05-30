7edec838f5a1b917428f847a8f72036906d732c7
Ignacio <ilopez-amor@ya.com>
2020-05-30 11:24:57 +0200

    Add types & props & change names

--- app/utils/types.ts
+++ app/utils/types.ts
@@ -1,9 +1,9 @@
-import { Color } from "tns-core-modules/color/color"
+import { Color } from 'tns-core-modules/color/color'
 
 export interface InitialCoordinates {
   lat: string
   lng: string
- }
+}
 
 export interface Coordinates {
   lat: number
@@ -16,33 +16,38 @@ export interface Circle {
   numberOfPoints: number
 }
 
-export interface PointsCoordinates {
+export interface Azimuth {
   theta: number
   radius: number
   center: Coordinates
 }
 
 export interface Marker extends Coordinates {
-  id: string;
-  title?: string;
-  subtitle?: string;
-  icon?: string;
-  iconPath?: string;
-  onTap?: Function;
-  onCalloutTap?: Function;
-  selected?: boolean;
-  update?: (newSettings: Marker) => void;
-  ios?: any;
-  android?: any;
+  id: string
+  group?: string
+  title?: string
+  subtitle?: string
+  icon?: string
+  iconPath?: string
+  onTap?: Function
+  onCalloutTap?: Function
+  selected?: boolean
+  isFollowed?: boolean
+  update?: (newSettings: Marker) => void
+  ios?: any
+  android?: any
 }
 
-export interface AddPolygonOptions {
-id: any
-points: Coordinates[]
-fillColor?: string | Color
-fillOpacity?: number
-strokeColor?: string | Color
-strokeWidth?: number
-strokeOpacity?: number
-isVisible?: boolean
+export interface PolygonOptions {
+  id: string
+  group?: string
+  points?: Coordinates[]
+  radius?: number
+  fillColor?: string | Color
+  fillOpacity?: number
+  oldFillOpacity?: number
+  strokeColor?: string | Color
+  strokeWidth?: number
+  strokeOpacity?: number
+  isVisible?: boolean
 }
