db8dccbaa7da0c9ee8434fce07f10267c791b872
Ignacio <ilopez-amor@ya.com>
2020-05-31 19:29:22 +0200

    Add action bar navigation

--- app/views/AppNavigator.vue
+++ app/views/AppNavigator.vue
@@ -1,20 +1,45 @@
 <template>
-  <Page>
+  <Page actionBarHidden="false">
     <ActionBar>
+      <!-- TODO: cambiar titulos por iconos -->
       <GridLayout
         width="100%"
-        columns="auto, *"
+        columns="auto, *, auto, auto, auto"
       >
+        <!-- TODO: icono hamburguesa -->
         <Label
+          class="actionBar_drawerMenu-button"
           text="MENU"
           col="0"
           @tap="$refs.drawer.nativeView.showDrawer()"
         />
         <Label
-          class="title"
+          class="actionBar_title"
           text="AREAS DE CONTROL"
           col="1"
         />
+        <!-- TODO: visibilidad de las areas de control -->
+        <!-- TODO: icono ojo - cambia según selección -->
+        <Label
+          class="actionBar_visibility-button"
+          text="VIS"
+          col="2"
+          @tap="onTapVisibility()"
+        />
+        <!-- TODO: icono lupa -->
+        <Label
+          class="actionBar_search-button"
+          text="SEARCH"
+          col="3"
+          @tap="onTapSearch()"
+        />
+        <!-- TODO: icono tres puntos -->
+        <!-- <Label
+          class="actionBar_overflowMenu-button"
+          text="OVERFLOW MENU"
+          col="4"
+          @tap="onTapOverflowMnu()"
+        /> -->
       </GridLayout>
     </ActionBar>
 
@@ -25,7 +50,7 @@
       >
         <Label
           class="drawer-header"
-          text="Drawer"
+          text="USUARIO"
         />
 
         <Label
@@ -46,8 +71,60 @@
         ~mainContent
       >
         <Frame>
-          <Map />
+          <Map 
+            :isVisible="isVisible"
+          />
         </Frame>
+        <!-- <BottomNavigation selectedIndex="0">
+          <TabStrip>
+            <TabStripItem>
+              <Label text="Map" />
+              <Image
+                src="font://&#xf015;"
+                class="fas"
+              />
+            </TabStripItem>
+
+            <TabStripItem class="special">
+              <Label text="Settings" />
+              <Image
+                src="font://&#xf007;"
+                class="fas"
+              />
+            </TabStripItem>
+
+
+            <TabStripItem class="messages">
+              <Label text="Messages" />
+              <Image
+                src="font://&#xf00e;"
+                class="fas"
+              />
+            </TabStripItem>
+          </TabStrip>
+
+          <TabContentItem>
+            <Frame>
+              <Map />
+            </Frame>
+          </TabContentItem>
+          <TabContentItem>
+            <GridLayout>
+              <Label
+                text="Settings Page"
+                class="h2 text-center"
+              />
+            </GridLayout>
+          </TabContentItem>
+          <TabContentItem>
+            <GridLayout>
+              <Label
+                text="Messages Page"
+                class="h2 text-center"
+              />
+            </GridLayout>
+          </TabContentItem>
+        </BottomNavigation> -->
       </StackLayout>
     </RadSideDrawer>
   </Page>
@@ -62,8 +139,19 @@ export default {
   },
   data() {
     return {
+      isVisible: true
     }
   },
+  methods: {
+    onTapVisibility() {
+      console.log("Change control areas visibility")
+      this.isVisible = !this.isVisible
+      
+    },
+    onTapSearch() {
+      console.log("Tap search action!")
+    },
+  }
 }
 </script>
 
