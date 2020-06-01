7e8a99f28a8dca5df3f78b615ce98e611b8296f0
Ignacio <ilopez-amor@ya.com>
2020-06-01 08:06:29 +0200

    New component

---
+++ app/components/UI/BottomAppBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <GridLayout 
+    rows="1" 
+    columns="1"
+  >
+    <Label 
+      text="BOTTOM APP BAR" 
+      textWrap="true" 
+    />
+  </GridLayout>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'BottomAppBar'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app/components/UI/FontIcon.vue
@@ -0,0 +1,40 @@
+<template>
+  <Label
+    :class="type"
+    :color="color"
+    :fontSize="fontSize"
+    :text="name | fonticon"
+    :backgroundColor="backgroundColor"
+    textAlignment="center"
+    @tap="$emit('tap')"
+  />
+</template>
+<script lang="ts">
+export default {
+  name: 'FontIcon',
+  props: {
+    type: {
+      type: String,
+      required: true,
+      default: 'fa'
+    },
+    color: {
+      type: String,
+      default: 'black'
+    },
+    fontSize: {
+      type: [String, Number],
+      default: 24,
+      validation: s => !isNaN(s)
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    backgroundColor: {
+      type:String,
+      default: 'transparent'
+    }
+  }
+}
+</script>
