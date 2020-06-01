ddc08d52922f738c87deb401afc62f7ef83c5c02
Ignacio <ilopez-amor@ya.com>
2020-06-01 20:38:43 +0200

    Added components

--- app/components/UI/BottomAppBar.vue
+++ app/components/UI/BottomAppBar.vue
@@ -1,21 +1,58 @@
 <template>
-  <GridLayout 
-    rows="1" 
-    columns="1"
+  <GridLayout
+    class="bottomAppBar"
+    orientation="horizontal"
+    height="56"
+    columns="auto, *, auto"
   >
-    <Label 
-      text="BOTTOM APP BAR" 
-      textWrap="true" 
+    <Label  
+      col="0"
+      text="Nuevo"
+      horizontalAlignment="center"
+      @tap="tapNewSecurityArea()"
+    />
+    <Label
+      col="1"
+      text="Borrar"
+      horizontalAlignment="center"
+      @tap="tapRemoveSecurityArea()"
+    />
+    <Label
+      col="2"
+      text="Centrar"
+      horizontalAlignment="center"
+      @tap="tapSetCenter()"
     />
   </GridLayout>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'BottomAppBar'
+  name: 'BottomAppBar',
+  methods: {
+    tapNewSecurityArea() {
+      this.$root.$emit('newSecurityArea', { name: 'user' })
+    },
+    tapRemoveSecurityArea() {
+      this.$root.$emit('removeSecurityArea', { name: 'user' })
+    }, 
+    tapSetCenter() {
+      this.$root.$emit('setCenter')
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  Label {
+    font-size: 20;
+    padding: {
+      top: 16;
+      right: 16;
+      left: 16;
+    }
+  }
+  .bottomAppBar {
+    background-color: purple;
+  }
 </style>
