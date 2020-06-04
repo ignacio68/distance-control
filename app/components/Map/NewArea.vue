d4d2b0d76a6bd9eb80a81fa2f48f34f5807ef70e
Ignacio <ilopez-amor@ya.com>
2020-06-04 19:22:03 +0200

    Add new component

---
+++ app/components/Map/NewArea.vue
@@ -0,0 +1,36 @@
+<template>
+  <GridLayout 
+    rows="*"
+  >
+    <CustomSlider
+      sliderName="radius"
+      :minValue="0"
+      :maxValue="100"
+      :value="radius"
+      @onIndexChanged="onIdexChanged()"
+    />
+  </GridLayout>
+</template>
+
+<script lang="ts">
+import CustomSlider from '@/components/UI/CustomSlider.vue'
+export default {
+  name: 'NewArea',
+  components: {
+    CustomSlider
+  },
+  data(){
+    return {
+      radius: 0
+    }
+  },
+  methods: {
+    onIndexChanged(value) {
+      this.$emit('inRadiusChange', value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
