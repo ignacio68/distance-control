d5d969c3f69d5cd3ea79513c147a0d4e8a891651
Ignacio <ilopez-amor@ya.com>
2020-06-04 12:28:39 +0200

    New component

--- app/components/Map/NewMarker.vue
+++ app/components/Map/NewMarker.vue
@@ -1,42 +1,70 @@
 <template>
   <GridLayout
-    rows="1"
-    columns="1"
+    rows="56, 56, *"
+    columns="*"
   >
-    <StackLayout orientation="horizontal">
-      <Label
-        text="nombre"
-        textWrap="true"
-      />
-      <TextField
-        v-model="markerName"
-        :editable="true"
-        :maxLength="16"
-        keyboardType="url"
-        returnKeyType="done"
-        :text="markerName"
-        @textChange="ontextChange()"
-      />
-    </StackLayout>
+    <TextForm
+      row="0"
+      class="myTextForm"
+      :labelWidth="80"
+      :textFieldWidth="200"
+      :returnKeyType="next"
+      textFormLabel="nombre:"
+      :value="values.id"
+      @onTextChange="setId(values.id)"
+    />
+    <TextForm
+      row="1"
+      class="myTextForm"
+      :labelWidth="80"
+      :textFieldWidth="200"
+      textFormLabel="grupo:"
+      :value="values.group"
+      @onTextChange="setGroup(values.group)"
+    />
+    <Button 
+      row="2"
+      text="Añadir" 
+      @tap="onTap" 
+    />
   </GridLayout>
 </template>
 
 <script lang="ts">
+import TextForm from '@/components/UI/TextForm.vue'
 export default {
   name: 'NewMarker',
+  components: {
+    TextForm
+  },
   data() {
     return {
-      markerName: null
+      values: {
+        id: "pepe",
+        group: ""
+      }
     }
   },
   methods: {
-    onTextChange() {
-      this.emit('textChange', this.markerName)
+    setId(id) {
+      console.log('setId()')
+      this.values.id = id
+    },
+    setGroup(group) {
+      console.log('setgroup()')
+      this.values.group = group
+    },
+    onTap() {
+      console.log('onTap()')
+      this.$emit('onMarkerDone', this.values)
+      // this.$emit('onMarkerDone')
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-
+ .myTextForm {
+  border: 3 solid green;
+}
 </style>
