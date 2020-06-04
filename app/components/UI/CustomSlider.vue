a08812a2ea45c1b9255aa2349e948c0612a37cfa
Ignacio <ilopez-amor@ya.com>
2020-06-04 12:29:01 +0200

    New component

---
+++ app/components/UI/CustomSlider.vue
@@ -0,0 +1,58 @@
+<template>
+  <GridLayout
+    rows="*"
+    columns="auto, auto, *, auto"
+  >
+    <Label
+      col="0"
+      :text="sliderName"
+      textWrap="true"
+    />
+    <Label
+      col="1"
+      :text="minValue"
+      textWrap="true"
+    />
+    <Slider
+      col="2"
+      :minValue="minValue"
+      :maxValue="maxValue"
+      :value="value"
+      @valueChange="$emit('ontapDraweMenu', value)"
+    />
+    <Label
+      col="3"
+      :text="maxValue"
+      textWrap="true"
+    />
+  </GridLayout>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'CustomSlider',
+  props: {
+    sliderName: {
+      type: String,
+      default: '',
+    },
+    minValue: {
+      type: Number,
+      default: 0,
+      validation: (s) => !isNaN(s),
+    },
+    maxValue: {
+      type: Number,
+      default: 1,
+      validation: (s) => !isNaN(s),
+    },
+    value: {
+      type: Number,
+      default: 0,
+      validation: (s) => !isNaN(s),
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
