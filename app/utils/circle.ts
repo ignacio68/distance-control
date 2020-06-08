70be33ff206b2ea25431bb39d17133a98a533986
Ignacio <ilopez-amor@ya.com>
2020-06-08 09:12:51 +0200

    Added location in circle function

--- app/utils/circle.ts
+++ app/utils/circle.ts
@@ -9,11 +9,12 @@ import { Coordinates, Circle, Azimuth, LocationInCircle } from './types'
 const PI = Math.PI
 const EARTH_RADIUS = 6378 // km
 
-const getPointCoordinates = (options: Azimuth) => {
-  const { theta, radius, center } = options
-  
+const getPointCoordinates = ({ theta, radius, center }: Azimuth) => {
+   
   const dy = Math.sin(radians(theta)) * radius
   const dx = Math.cos(radians(theta)) * radius
+  // const dx = radius / (111.320 * Math.cos(radians(center.lat)))
+  // const dy = radius / 110.574
   const newLatitude = center.lat + degrees(dy / EARTH_RADIUS) 
   const newLongitude =
     center.lng + (degrees(dx / EARTH_RADIUS)) / Math.cos(radians((center.lat * PI) / 180))
@@ -23,8 +24,7 @@ const getPointCoordinates = (options: Azimuth) => {
   return coordinates
 }
 
-const getCirclePointsCoordinates = (options: Circle) => {
-  const { center, radius, numberOfPoints } = options
+const getCirclePointsCoordinates = ({ center, radius, numberOfPoints }: Circle) => {
   console.log('getCirclePointsCoordinates()')
 
   const thetasList = range(0, 360, 360 / numberOfPoints)
@@ -36,24 +36,21 @@ const getCirclePointsCoordinates = (options: Circle) => {
   return allPointsCoordinates  
 }
 
-
-
   /**
   * Is the current location within the given circle? 
   *
-  * @param {number} longitude to check
-  * @param {number} latitude to check
-  * @param {number} longitude center of circle
-  * @param {number} latitude center of circle
-  * @param {number} radius of circle in meters
+  * @param lng{number} longitude to check
+  * @param lat{number} latitude to check
+  * @param circleLng{number} longitude center of circle
+  * @param circleLat{number} latitude center of circle
+  * @param circleRadius{number} radius of circle in meters
   *
   * @return {boolean} true if the point is within the given geofence.
   *
   * @link https://stackoverflow.com/questions/24680247/check-if-a-latitude-and-longitude-is-within-a-circle-google-maps
   */
-// lng: number, lat: number, circleLng: number, circleLat: number, circleRadius: number
-const isLocationInCircle = (options: LocationInCircle ) => {
-  const { lng, lat, circleLng, circleLat, circleRadius } = options 
+
+const isLocationInCircle = ({ lng, lat, circleLng, circleLat, circleRadius }: LocationInCircle ) => {
   const ky = 111.11111111
   const kx = Math.cos( Math.PI * circleLat / 180.0 ) * ky
   const dx = Math.abs( circleLng - lng ) * kx
