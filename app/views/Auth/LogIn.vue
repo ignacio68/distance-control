9ea1a0bafdca5bcb0219082323d917ccecc98568
Ignacio <ilopez-amor@ya.com>
2020-06-08 09:09:05 +0200

    New authoritation pages

---
+++ app/views/Auth/LogIn.vue
@@ -0,0 +1,15 @@
+<template>
+  <Page>
+  </Page>
+</template>
+
+<script lang="ts">
+export default {
+  name:"LogIn"
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app/views/Auth/SignUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <Page>
+  </Page>
+</template>
+
+<script lang="ts">
+export default {
+  name:"SignUp"
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
