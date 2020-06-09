18b96f0ce31606ccda4dad334e4d2ef9386121f7
Ignacio <ilopez-amor@ya.com>
2020-06-09 18:34:24 +0200

    Created only for testing propouses

---
+++ app/fake/FakeComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <StackLayout>
+    <Label
+      :text="msg"
+      textWrap="true"
+      color="black"
+    />
+  </StackLayout>
+</template>
+
+<script lang="ts">
+export default {
+  name:'FakeComponent',
+  props: {
+    msg:{
+      type: String,
+      default: 'FAKE COMPONENT'
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app/fake/FakePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <Frame>
+    <Page>
+      <StackLayout>
+        <Label
+          text="FAKE PAGE"
+          textWrap="true"
+        />
+      </StackLayout>
+    </Page>
+  </Frame>
+</template>
+
+<script lang="ts">
+export default {
+  name:'FakePage'
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
