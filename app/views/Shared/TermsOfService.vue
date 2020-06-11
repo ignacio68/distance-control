dca9861e468a2718475d31ab453a0e5c24bc9045
Ignacio <ilopez-amor@ya.com>
2020-06-11 12:01:20 +0200

    Improve: added Terms of Service page

---
+++ app/views/Shared/TermsOfService.vue
@@ -0,0 +1,35 @@
+<template>
+  <Page>
+    <ActionBar title="Terms of Service">
+      <NavigationButton
+        android.systemIcon="ic_menu_back"
+        @tap="$navigateBack"
+      />
+    </ActionBar>
+    <StackLayout>
+      <Label
+        class="text"
+        text="Terms of Service page"
+        textWrap="$navigateBack"
+      />
+    </StackLayout>
+  </Page>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'TermsOfService',
+  data() {
+    return {}
+  }
+})
+</script>
+<style lang="scss" scoped>
+.text {
+  vertical-align: center;
+  text-align: center;
+  margin-top: 16;
+  font-size: 16;
+}
+</style>
