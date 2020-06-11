be079332725f13683993a53e7511a69f541ca912
Ignacio <ilopez-amor@ya.com>
2020-06-11 12:04:20 +0200

    Added welcome page locales

--- app/locales/i18n/en_US/views.ts
+++ app/locales/i18n/en_US/views.ts
@@ -1,4 +1,11 @@
 export default {
-  map: {
+  welcome: {
+    title: "WELCOME!",
+    subtitle: "Load your security areas ",
+    phone: "We need your phone number to sign up you and you can connect with your family and friends and storage your preferences",
+    accept: "If you continue, you accept our ",
+    privacy: "Privacy Policy",
+    and: "and",
+    service: "Terms of Service"
   }
 }
--- app/locales/i18n/es_ES/views.ts
+++ app/locales/i18n/es_ES/views.ts
@@ -1,4 +1,11 @@
 export default {
-  map: {
+  welcome: {
+    title: "¡BIENVENIDO!",
+    subtitle: "Activa tus zonas de seguridad",
+    phone: "Necesitamos tu numero de teléfono para darte de alta y que puedas conectarte con tus familiares y amigos y guardar tus preferencias",
+    accept: "Si continuas, aceptas nuestra",
+    privacy: "Política de Privacidad",
+    and: "y",
+    service: "Terminos de Servicio"
   }
 }
