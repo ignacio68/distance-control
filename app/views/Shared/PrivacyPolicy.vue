9fd53bc54e11c93069270d74d6d0087c8a45e079
Ignacio <ilopez-amor@ya.com>
2020-06-11 12:01:00 +0200

    Improve: added Privacy Policy page

---
+++ app/views/Shared/PrivacyPolicy.vue
@@ -0,0 +1,36 @@
+<template>
+  <Page>
+    <ActionBar title="Privacy Policy">
+      <!-- TODO: areglar el uso de iconos -->
+      <NavigationButton
+        android.systemIcon="ic_menu_back"
+        @tap="$navigateBack"
+      />
+    </ActionBar>
+    <StackLayout>
+      <Label
+        class="text"
+        text="Privacy Policy page"
+        textWrap="true"
+      />
+    </StackLayout>
+  </Page>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'PrivacyPolicy',
+  data() {
+    return {}
+  }
+})
+</script>
+<style lang="scss" scoped>
+.text {
+  vertical-align: center;
+  text-align: center;
+  margin-top: 16;
+  font-size: 16;
+}
+</style>
