d56e0019aad58f031fc71bacee04812a0e334512
Ignacio <ilopez-amor@ya.com>
2020-06-17 12:24:59 +0200

    added phone number field text

--- app/locales/i18n/en_US/views.ts
+++ app/locales/i18n/en_US/views.ts
@@ -3,10 +3,11 @@ export default {
     title: "WELCOME!",
     subtitle: "Load your security areas ",
     phone: "We need your phone number to sign up you and you can connect with your family and friends and storage your preferences",
+    phoneNumber: "phone number",
     button: "Continue",
     accept: "If you continue, you accept our ",
     privacy: "Privacy Policy",
-    and: "and",
+    and: " and ",
     service: "Terms of Service"
   }
 }
--- app/locales/i18n/es_ES/views.ts
+++ app/locales/i18n/es_ES/views.ts
@@ -3,10 +3,11 @@ export default {
     title: "¡BIENVENIDO!",
     subtitle: "Activa tus zonas de seguridad",
     phone: "Necesitamos tu numero de teléfono para darte de alta y que puedas conectarte con tus familiares y amigos y guardar tus preferencias",
+    phoneNumber: "número de teléfono",
     button: "Continuar",
-    accept: "Si continuas, aceptas nuestra",
+    accept: "Si continuas, aceptas nuestra ",
     privacy: "Política de Privacidad",
-    and: "y",
+    and: " y ",
     service: "Terminos de Servicio"
   }
 }
