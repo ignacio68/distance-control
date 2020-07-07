ca7d553d5d487a9a90f0d45e4e67c276faaa5605
Ignacio <ilopez-amor@ya.com>
2020-07-07 10:31:29 +0200

    Removed Fab and IconFont plugins

--- app/plugins/Components.ts
+++ app/plugins/Components.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import BottomSheetPlugin from 'nativescript-material-bottomsheet/vue' 
-import { TNSFontIcon, fonticon } from 'nativescript-fonticon'
 import ButtonPlugin from 'nativescript-material-button/vue'
 import SliderPlugin from 'nativescript-material-slider/vue'
 import TextFieldPlugin from 'nativescript-material-textfield/vue'
@@ -9,25 +8,6 @@ const v = <any>Vue
 
 // Material Bottom Sheet
 v.use(BottomSheetPlugin)
-
-// TODO: Change to material design version
-v.registerElement(
-  'Fab',
-  () => require('@nstudio/nativescript-floatingactionbutton').Fab
-)
-
-// //  Font Icon
-// TNSFontIcon.debug = false
-// TNSFontIcon.paths = {
-//   fa: 'assets/styles/css/font-awesome/fontawesome.css',
-//   far: 'assets/styles/css/font-awesome/regular.css',
-//   fas: 'assets/styles/css/font-awesome/solid.css',
-//   fab: 'assets/styles/css/font-awesome/brands.css',
-//   // mdi: 'assets/styles/css/material-design/materialIcons.css',
-//   mdi: 'material-design-icons.css'
-// }
-// TNSFontIcon.loadCss()
-// v.filter('fonticon', fonticon)
 
 // Material Button
 v.use(ButtonPlugin)
