d4e53742de96d7e12229768f5049028e64e6ff14
Ignacio <ilopez-amor@ya.com>
2020-07-08 11:27:25 +0200

    Removed composition api

--- app/main.ts
+++ app/main.ts
@@ -20,19 +20,25 @@ import './plugins/Mapbox'
 import './plugins/Components'
 
 // composable 
-import { reactive, computed } from '@vue/composition-api'
-import  { useUser } from './composables/useUser'
-
-import AppNavigator from './views/AppNavigator.vue'
-import Welcome from './views/Welcome/Welcome.vue'
-// import FakePage from './fake/FakePage.vue'
+// import { reactive, computed } from '@vue/composition-api'
+// import  { useUser } from './composables/useUser'
+// import { getUser as user } from './store/user'
+import { getUser as user } from './store/user'
 
 import { isAndroid, isIOS } from 'tns-core-modules/platform'
 
 import VueDevtools from 'nativescript-vue-devtools'
 
+import AppNavigator from './views/AppNavigator.vue'
+import Welcome from './views/Welcome/Welcome.vue'
+import { userLogin } from './api'
+// import FakePage from './fake/FakePage.vue'
+// FIXME: initial view
+const navigation = () => user.isLogged ? AppNavigator : Welcome
+
 const v = <any>Vue
 declare const TNS_ENV: any
+
 
 if(TNS_ENV !== 'production') {
   // v.use(VueDevtools, { host: '192.168.1.101'}) //mobile
@@ -44,19 +50,17 @@ v.config.silent = (TNS_ENV === 'production')
  
 new v({
   i18n,
-  setup() {
-    const { isLogging } = useUser()
-    return { isLogging }
+  data() {
+    return{}
   },
   beforeCreate() {
     // Set the platform OS global variable
     v.prototype.IS_ANDROID = isAndroid
     v.prototype.IS_IOS = isIOS
-    console.log(`Running on Android? ${isAndroid}`)
-    console.log(`Running on iOS? ${isIOS}`)
 
     setLanguage()
   },
-  render: h => h('frame', [h(Welcome)])
-  // render: h => h('frame', [h( this.isLogging ? AppNavigator : Welcome)])
+  // render: h => h('frame', [h(navigation)])
+  render: h => h('frame', [h(AppNavigator)])
+  // render: h => h('frame', [h(Welcome)])
 }).$start()
