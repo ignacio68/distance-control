a78a19fec267102957d1678c74ccad30431e99a7
Ignacio <ilopez-amor@ya.com>
2020-07-09 11:49:08 +0200

    Removed export default

--- app/store/markers.ts
+++ app/store/markers.ts
@@ -1,21 +1,20 @@
 import Vue from 'Vue'
 
 const state = Vue.observable({
-    markers: []
+    markers: [],
 })
 
-const find = (id: any) => state.markers.find(marker => marker.id = id)
+const find = (id: string): string => state.markers.find(marker => marker.id = id)
 
-export const getMarker = (id: any) => find(id)
-
+export const getMarker = (id: string): string => find(id)
 export const setNewMarker = (marker) => state.markers.push(marker)
 export const deleteMarker = (id: string) => {
     // TODO: deploy the code
     const marker = find(id)
 }
 
-export default {
-    getMarker,
-    setNewMarker,
-    deleteMarker
-}
+// export default {
+//     getMarker,
+//     setNewMarker,
+//     deleteMarker
+// }
